365b6f4cb98d3316e76b9f13edec4215f0e08298
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-04-06 23:44:17 +0900

    return an individual slide

--- express/index.ts
+++ express/index.ts
@@ -1,12 +1,32 @@
-import { Request } from "express";
-import * as fs from "fs";
 import express from "express";
 
 const app = express();
 const port = 3038;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+type Slide = {
+  id: string
+}
+
+app.get("/slides/:slideId", async (req, res) => {
+  const dbRes = await fetch(
+    `http://localhost:3036/slides/${req.params.slideId}`
+  );
+
+  if (dbRes.status === 404) {
+    res.status(404);
+    res.send("Not Found");
+    return;
+  }
+
+  const json = await dbRes.json() as Slide;
+
+  res.json(json);
+});
+
+app.get("/slides", (req, res) => {
+  console.log(req.query);
+  const page = req.query["page"] as string;
+  res.send("slides " + page);
 });
 
 app.listen(port, () => {
